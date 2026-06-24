603541198ff5818d209edc0ba42bc58616f0a9df
Taku-Shimamoto <ithachioji31.562@gmail.com>
2026-06-24 15:18:48 +0900

    6/24
    ヘッダー、フッターのテンプレート化
    コピーボタン修正
    ボタンページ修正

--- common.js
+++ common.js
@@ -1,3 +1,51 @@
+// ==================================================
+// ヘッダーのテンプレート化
+// ==================================================
+
+customElements.define("site-header", class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <h1><a href="index.html">UI Dictionary</a></h1>
+                <div id="hamburger" class="hamburger">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <nav id="hamburger_nav">
+                    <ul>
+                        <li><a class="nav-link" href="#">ページの一番上に戻る</a></li>
+                        <li><a class="nav-link" href="#feature">特徴</a></li>
+                        <li><a class="nav-link" href="#code">ソースコードの一例</a></li>
+                        <li><a class="nav-link" href="#html_code">HTML</a></li>
+                        <li><a class="nav-link" href="#css_code">CSS</a></li>
+                        <li><a class="nav-link" href="#js_code">JavaScript</a></li>
+                        <li><a class="nav-link" href="#explanation">解説</a></li>
+                    </ul>
+                </nav>
+            </header>
+        `;
+    }
+});
+
+// ==================================================
+// フッターのテンプレート化
+// ==================================================
+
+customElements.define("site-footer", class extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+                Copyright &copy; 2026 改善委員会 - <a href="index.html">ホームへ戻る</a> -
+            </footer>
+        `;
+    }
+});
+
+// ==================================================
+// ハンバーガーメニュー
+// ==================================================
+
 const hamburgerButton = document.getElementById("hamburger");
 const navigationWindow = document.getElementById("hamburger_nav");
 const navigationLinks = Array.from(document.getElementsByClassName("nav-link"));
@@ -16,6 +64,10 @@ function clearClassActive() {
     navigationWindow.classList.remove("active");
 }
 
+// ==================================================
+// ページトップボタン
+// ==================================================
+
 const pageTopButton = document.getElementById("page_top");
 const BUTTON_APPEARANCE_HEIGHT = 300;
 
@@ -25,20 +77,23 @@ function toggleTopScrollButton() {
     pageTopButton.classList.toggle("is-visible", window.scrollY > BUTTON_APPEARANCE_HEIGHT);
 }
 
+// ==================================================
+// コードのコピー
+// ==================================================
+
 const copyButtons = Array.from(document.getElementsByClassName("copy-button"));
 const copyTexts = Array.from(document.getElementsByClassName("copy-text"));
-const scheduleTime = 3000;
-let timerId = null;
+const RETURN_MS = 3000;
 
 // テキストのコピー
 copyButtons.forEach((button, i) => button.addEventListener("click", () => copyText(i)));
 function copyText(i) {
-    copyButtons[i].value = "コピーしました。";
+    copyButtons[i].value = "✓";
     copyButtons[i].disabled = true;
     navigator.clipboard.writeText(copyTexts[i].textContent);
 
-    timerId = setTimeout(() => {
-        copyButtons[i].value = "コードをコピーする";
+    setTimeout(() => {
+        copyButtons[i].value = "Copy";
         copyButtons[i].disabled = false;
-    }, scheduleTime);
+    }, RETURN_MS);
 }
